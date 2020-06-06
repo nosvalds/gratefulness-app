237ed9fc74c9b0fe79d269036b2280071e79cec8
Nik Osvalds <nosvalds@gmail.com>
2020-06-06 16:57:04 +0100

    fix thought factory bug

--- database/factories/ThoughtFactory.php
+++ database/factories/ThoughtFactory.php
@@ -7,8 +7,8 @@ use Faker\Generator as Faker;
 
 $factory->define(Thought::class, function (Faker $faker) {
     return [
-        // post
-        'post' => $faker->sentence,
+        // A thought includes content and an author
+        'content' => $faker->sentence,
         'author' => $faker->firstName
     ];
 });
