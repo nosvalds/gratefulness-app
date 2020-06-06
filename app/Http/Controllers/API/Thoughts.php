7055bbb1f208172113b100560d78438463cf8e4e
Nik Osvalds <nosvalds@gmail.com>
2020-06-06 17:14:43 +0100

    try to create GET Api route and index

--- app/Http/Controllers/API/Thoughts.php
+++ app/Http/Controllers/API/Thoughts.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Thought;
 
 class Thoughts extends Controller
 {
@@ -14,7 +15,8 @@ class Thoughts extends Controller
      */
     public function index()
     {
-        //
+        // get all thoughts
+        return Thought::all();
     }
 
     /**
